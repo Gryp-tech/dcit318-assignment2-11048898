366f0ab8c8026593ee259ce0bd1b6a92466a91aa
Gryp-tech <eogyebi001@st.ug.edu.gh>
2025-08-07 14:13:12 +0000

    Implemented IMovable interface

---
+++ MyConsoleApp/InterfaceDemo.cs
@@ -0,0 +1,26 @@
+
+using System;
+
+namespace MyConsoleApp
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
